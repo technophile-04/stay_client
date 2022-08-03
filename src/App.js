678cf606a0ecfff1e4f9df23117eaac4f72819d8
Shiv Bhonde <shivbhonde04@gmail.com>
2022-08-03 12:48:51 +0530

    intialize

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Listings } from './sections';
+
+const App = () => {
+	return (
+		<div>
+			<Listings title="TheRent" />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import ApolloClient from "apollo-boost";
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./styles/index.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:9000/api",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+reportWebVitals();
