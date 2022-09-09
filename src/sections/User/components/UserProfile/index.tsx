845a66582999a556da93bf6568ddf2569b856d03
Shiv Bhonde <shivbhonde04@gmail.com>
2022-09-09 22:31:52 +0530

    add user basic info

---
+++ src/sections/User/components/UserProfile/index.tsx
@@ -0,0 +1,31 @@
+import { Avatar, Card, Divider, Typography } from "antd";
+
+import { User as UserData } from "../../../../lib/graphql/queries/User/__generated__/user";
+
+interface Props {
+  user: UserData["user"];
+}
+
+const { Paragraph, Text, Title } = Typography;
+
+export const UserProfile = ({ user }: Props) => {
+  return (
+    <div className="user-profile">
+      <Card className="user-profile__card">
+        <div className="user-profile__avatar">
+          <Avatar size={100} src={user.avatar} />
+        </div>
+        <Divider />
+        <div className="user-profile__details">
+          <Title level={4}>Details</Title>
+          <Paragraph>
+            Name: <Text strong>{user.name}</Text>
+          </Paragraph>
+          <Paragraph>
+            Contact: <Text strong>{user.contact}</Text>
+          </Paragraph>
+        </div>
+      </Card>
+    </div>
+  );
+};
--- src/sections/User/index.tsx
+++ src/sections/User/index.tsx
@@ -1,3 +1,35 @@
+import { Col, Row } from "antd";
+import { Content } from "antd/lib/layout/layout";
+import { useQuery } from "react-apollo";
+import { useParams } from "react-router-dom";
+import { USER } from "../../lib/graphql";
+import { User as UserData, UserVariables } from "../../lib/graphql/queries/User/__generated__/user";
+import { UserProfile } from "./components";
+
+type UserParams = {
+  id: string;
+};
+
 export const User = () => {
-  return <div>User</div>;
+  const { id } = useParams<UserParams>();
+
+  const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
+    variables: {
+      id: id!,
+    },
+  });
+
+  const user = data ? data.user : null;
+
+  const userProfileElement = user ? <UserProfile user={user} /> : null;
+
+  console.log(data);
+
+  return (
+    <Content className="user">
+      <Row gutter={12} justify="space-between">
+        <Col xs={24}>{userProfileElement}</Col>
+      </Row>
+    </Content>
+  );
 };
